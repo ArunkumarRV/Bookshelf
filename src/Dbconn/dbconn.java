97f026afd29f214c761dec3ce7fa40a971b88f1a
Vinitha <146748375+ArunkumarRV@users.noreply.github.com>
2025-05-28 11:02:21 +0530

    Create dbconn.java

---
+++ src/Dbconn/dbconn.java
@@ -0,0 +1,20 @@
+package DBcon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbconn {
+
+	static Connection con;
+
+	public static Connection create(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");  
+				con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ecom","root","root");	 
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();     
+		}
+		return con;
+	
+}}
